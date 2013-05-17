427f91442e3f2d3aee63d0e8030596200241a397
Markus Mottl <markus.mottl@gmail.com>
2013-05-17 10:17:36 -0400

    Fixed bug in combi example

--- examples/combi_ex.ml
+++ examples/combi_ex.ml
@@ -9,7 +9,7 @@ let print_arr arr =
 
 let () =
   let c = Combi.make 4 2 in
-  for _i = 0 to int_of_float (Sf.choose 4 2) do
+  for _i = 1 to int_of_float (Sf.choose 4 2) do
     print_arr (Combi.to_array c);
     Combi.next c
   done
