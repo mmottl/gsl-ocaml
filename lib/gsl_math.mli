a06da6cea36d683342198da3463fd3b4b6f75b7e
Markus Mottl <markus.mottl@gmail.com>
2015-11-17 12:50:37 -0500

    Fixed comment warnings

--- lib/gsl_math.mli
+++ lib/gsl_math.mli
@@ -7,22 +7,39 @@
 (** {3 Constants} *)
 
 val e          : float (** e *)             
+
 val log2e      : float (** log_2 (e) *)     
+
 val log10e     : float (** log_10 (e) *)    
+
 val sqrt2      : float (** sqrt(2) *)       
+
 val sqrt1_2    : float (** sqrt(1/2) *)     
+
 val sqrt3      : float (** sqrt(3) *)       
+
 val pi         : float (** pi *)            
+
 val pi_2       : float (** pi/2 *)          
+
 val pi_4       : float (** pi/4 *)          
+
 val sqrtpi     : float (** sqrt(pi) *)      
+
 val i_2_sqrtpi : float (** 2/sqrt(pi) *)    
+
 val i_1_pi     : float (** 1/pi *)          
+
 val i_2_pi     : float (** 2/pi *)          
+
 val ln10       : float (** ln(10) *)        
+
 val ln2        : float (** ln(2) *)         
+
 val lnpi       : float (** ln(pi) *)        
+
 val euler      : float (** Euler constant *)
+
 
 (** {3 Simple Functions} *)
 
